44f51c0e536f70abd9136847c800e866bc04dd6d
unknown <phucthinh291203>
2024-11-01 16:09:01 +0700

    new score

--- gen/protos/scoreNew_grpc.pb.go
+++ gen/protos/scoreNew_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.28.3
-// source: protos/score.proto
+// source: protos/scoreNew.proto
 
 package proto
 
@@ -117,5 +117,5 @@ var ScoreService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "protos/score.proto",
+	Metadata: "protos/scoreNew.proto",
 }
