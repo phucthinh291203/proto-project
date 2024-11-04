bbf834d7f7fdfb1dc36f9627aea411cb53845018
unknown <phucthinh291203>
2024-11-04 09:36:03 +0700

    new score

--- gen/protos/scoreNew_grpc.pb.go
+++ gen/protos/scoreNew_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -20,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion9
 
 const (
 	ScoreService_CreateNewScore_FullMethodName = "/ScoreService/CreateNewScore"
+	ScoreService_GetAllScore_FullMethodName    = "/ScoreService/GetAllScore"
 )
 
 // ScoreServiceClient is the client API for ScoreService service.
@@ -27,6 +29,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ScoreServiceClient interface {
 	CreateNewScore(ctx context.Context, in *CreateScoreRequest, opts ...grpc.CallOption) (*CreateScoreResponse, error)
+	GetAllScore(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetAllScoreStudentResponse, error)
 }
 
 type scoreServiceClient struct {
@@ -47,11 +50,22 @@ func (c *scoreServiceClient) CreateNewScore(ctx context.Context, in *CreateScore
 	return out, nil
 }
 
+func (c *scoreServiceClient) GetAllScore(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetAllScoreStudentResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(GetAllScoreStudentResponse)
+	err := c.cc.Invoke(ctx, ScoreService_GetAllScore_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ScoreServiceServer is the server API for ScoreService service.
 // All implementations must embed UnimplementedScoreServiceServer
 // for forward compatibility.
 type ScoreServiceServer interface {
 	CreateNewScore(context.Context, *CreateScoreRequest) (*CreateScoreResponse, error)
+	GetAllScore(context.Context, *emptypb.Empty) (*GetAllScoreStudentResponse, error)
 	mustEmbedUnimplementedScoreServiceServer()
 }
 
@@ -64,6 +78,9 @@ type UnimplementedScoreServiceServer struct{}
 
 func (UnimplementedScoreServiceServer) CreateNewScore(context.Context, *CreateScoreRequest) (*CreateScoreResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateNewScore not implemented")
+}
+func (UnimplementedScoreServiceServer) GetAllScore(context.Context, *emptypb.Empty) (*GetAllScoreStudentResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAllScore not implemented")
 }
 func (UnimplementedScoreServiceServer) mustEmbedUnimplementedScoreServiceServer() {}
 func (UnimplementedScoreServiceServer) testEmbeddedByValue()                      {}
@@ -104,6 +121,24 @@ func _ScoreService_CreateNewScore_Handler(srv interface{}, ctx context.Context, 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ScoreService_GetAllScore_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ScoreServiceServer).GetAllScore(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ScoreService_GetAllScore_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ScoreServiceServer).GetAllScore(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ScoreService_ServiceDesc is the grpc.ServiceDesc for ScoreService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +149,10 @@ var ScoreService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CreateNewScore",
 			Handler:    _ScoreService_CreateNewScore_Handler,
+		},
+		{
+			MethodName: "GetAllScore",
+			Handler:    _ScoreService_GetAllScore_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
