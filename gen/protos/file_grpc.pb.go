7f558aefbe3aea4d6a46fa0e7121911e562dc56c
unknown <phucthinh291203>
2024-11-07 14:43:43 +0700

    update file

--- gen/protos/file_grpc.pb.go
+++ gen/protos/file_grpc.pb.go
@@ -19,7 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	FileService_ImportFile_FullMethodName = "/FileService/ImportFile"
+	FileService_ImportFile_FullMethodName      = "/FileService/ImportFile"
+	FileService_GetFileToUpdate_FullMethodName = "/FileService/GetFileToUpdate"
 )
 
 // FileServiceClient is the client API for FileService service.
@@ -27,6 +28,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type FileServiceClient interface {
 	ImportFile(ctx context.Context, in *ImportFileRequest, opts ...grpc.CallOption) (*ImportFileResponse, error)
+	GetFileToUpdate(ctx context.Context, in *GetFileToUpdateRequest, opts ...grpc.CallOption) (*GetFileToUpdateResponse, error)
 }
 
 type fileServiceClient struct {
@@ -47,11 +49,22 @@ func (c *fileServiceClient) ImportFile(ctx context.Context, in *ImportFileReques
 	return out, nil
 }
 
+func (c *fileServiceClient) GetFileToUpdate(ctx context.Context, in *GetFileToUpdateRequest, opts ...grpc.CallOption) (*GetFileToUpdateResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(GetFileToUpdateResponse)
+	err := c.cc.Invoke(ctx, FileService_GetFileToUpdate_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // FileServiceServer is the server API for FileService service.
 // All implementations must embed UnimplementedFileServiceServer
 // for forward compatibility.
 type FileServiceServer interface {
 	ImportFile(context.Context, *ImportFileRequest) (*ImportFileResponse, error)
+	GetFileToUpdate(context.Context, *GetFileToUpdateRequest) (*GetFileToUpdateResponse, error)
 	mustEmbedUnimplementedFileServiceServer()
 }
 
@@ -64,6 +77,9 @@ type UnimplementedFileServiceServer struct{}
 
 func (UnimplementedFileServiceServer) ImportFile(context.Context, *ImportFileRequest) (*ImportFileResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ImportFile not implemented")
+}
+func (UnimplementedFileServiceServer) GetFileToUpdate(context.Context, *GetFileToUpdateRequest) (*GetFileToUpdateResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetFileToUpdate not implemented")
 }
 func (UnimplementedFileServiceServer) mustEmbedUnimplementedFileServiceServer() {}
 func (UnimplementedFileServiceServer) testEmbeddedByValue()                     {}
@@ -104,6 +120,24 @@ func _FileService_ImportFile_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _FileService_GetFileToUpdate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetFileToUpdateRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FileServiceServer).GetFileToUpdate(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: FileService_GetFileToUpdate_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FileServiceServer).GetFileToUpdate(ctx, req.(*GetFileToUpdateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // FileService_ServiceDesc is the grpc.ServiceDesc for FileService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +148,10 @@ var FileService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ImportFile",
 			Handler:    _FileService_ImportFile_Handler,
+		},
+		{
+			MethodName: "GetFileToUpdate",
+			Handler:    _FileService_GetFileToUpdate_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
